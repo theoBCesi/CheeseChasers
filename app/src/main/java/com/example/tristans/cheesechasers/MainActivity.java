c3b822d0f58e705d40249dcd95b4a7431815b36a
Theo Bouchevreau <theo.bouchevreau@viacesi.fr>
2018-10-10 15:14:51 +0200

    context

--- app/src/main/java/com/example/tristans/cheesechasers/MainActivity.java
+++ app/src/main/java/com/example/tristans/cheesechasers/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lecontext=this;
         Button b = findViewById(R.id.NewGame);
         b.setOnClickListener(new View.OnClickListener() {
             @Override
