f1745a7efb1af8282bcb8f62b7bcfcac3458923c
Theo Bouchevreau <theo.bouchevreau@viacesi.fr>
2018-10-10 17:01:33 +0200

    affichage cercle sur dessin

--- app/src/main/java/com/example/tristans/cheesechasers/Dessin.java
+++ app/src/main/java/com/example/tristans/cheesechasers/Dessin.java
@@ -4,57 +4,60 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import java.util.LinkedList;
+import java.util.List;
 
 public class Dessin extends View implements View.OnTouchListener {
 
-    LinkedList<Cercle> cercles;
+    //LinkedList<Cercle> cercles;
+    Case[][] jeux = new Case[5][10];
+
     public Dessin(Context context, AttributeSet attrs) {
         super(context, attrs);
-        cercles = new LinkedList<Cercle>();
         this.setOnTouchListener(this);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        for (Cercle c : cercles) {
-            c.draw(canvas);
-        }
-        Log.d("ee",""+this.getWidth()+"-"+this.getHeight());
+        Log.d("onDraw", "" + this.getWidth() + "-" + this.getHeight());
+        Case c =new Case(100,100,100);
+        c.draw(canvas);
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        int x = (int) event.getX();
-        int y = (int) event.getY();
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                cercles.add(new Cercle(x, y, 1));
-                break;
-            case MotionEvent.ACTION_MOVE:
-                Cercle clast = cercles.getLast();
-                clast.rayon = (int) Math.sqrt((x - clast.xc) * (x - clast.xc) + (y - clast.yc) * (y - clast.yc));
-                break;
-        }
-        this.invalidate();
+        //Log.d("onTouch", "x: " + event.getX() + " - y: " + event.getY());
+
+        //switch (event.getAction()) {
+        //    case MotionEvent.ACTION_DOWN:
+        //        Case c = new Case(1, 100, 100);
+        //        jeux[0][1] = c;
+        //        break;
+        //}
+        //this.invalidate();
         return true;
     }
 }
 
-class Cercle {
-    int xc, yc, rayon;
+
+class Case {
+    int type;
+    int largeur;
+    int hauteur;
     private Paint paint;
 
-    public Cercle(int x, int y, int r) {
-        xc = x;
-        yc = y;
-        rayon = r;
-        paint = new Paint();
+    public Case(int type, int largeur, int hauteur) {
+        this.type = type;
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+        this.paint = new Paint();
+        paint.setStrokeWidth(3);
         paint.setColor(Color.rgb((int) (Math.random() * 255),
                 (int) (Math.random() * 255),
                 (int) (Math.random() * 255))
@@ -62,6 +65,9 @@ class Cercle {
     }
 
     public void draw(Canvas canvas) {
-        canvas.drawCircle(xc, yc, rayon, paint);
+        Log.d("drawCase","draw");
+        canvas.drawCircle(100, 100, 50, paint);
+        //canvas.drawRect(20, 20, 20, 20, paint);
+        //canvas.drawBitmap(getRessource());
     }
 }
