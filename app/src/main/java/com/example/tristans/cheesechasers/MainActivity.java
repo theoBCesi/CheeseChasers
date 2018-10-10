7da144dd65eb83225b2580af31278cc0c6596691
Theo Bouchevreau <theo.bouchevreau@viacesi.fr>
2018-10-10 15:11:22 +0200

    bouton nouvelle partie

--- app/src/main/java/com/example/tristans/cheesechasers/MainActivity.java
+++ app/src/main/java/com/example/tristans/cheesechasers/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.tristans.cheesechasers;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
+    Activity lecontext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = findViewById(R.id.NewGame);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent defineIntent = new Intent(lecontext, pagegame.class);
+                //lecontext.startActivity(defineIntent);
+            }
+        });
     }
 }
